365f73f9b015534b04061f0313b95055bcf27f37
docent <kldm-2@mail.ru>
2024-11-30 12:41:41 +0400

    Completed step four V1.0

--- app/src/main/java/hexlet/code/App.java
+++ app/src/main/java/hexlet/code/App.java
@@ -2,6 +2,11 @@ package hexlet.code;
 
 import picocli.CommandLine;
 import picocli.CommandLine.Command;
+import picocli.CommandLine.Option;
+import picocli.CommandLine.Parameters;
+
+import java.io.File;
+import java.util.concurrent.Callable;
 
 public class App {
     public static void main(String[] args) {
@@ -9,11 +14,29 @@ public class App {
     }
 }
 
+//https://picocli.info/#_usage_help
+
 @Command(name = "gendiff",
         mixinStandardHelpOptions = true,
-        description = "Compares two configuration files and shows a difference.")
+        description = "Compares two configuration files and shows a difference.",
+        version = "Differ 1.0")
 
-class MyHelp implements Runnable {
-    public void run() {
+
+class MyHelp implements Callable<Integer> {
+    @Override
+    public Integer call() throws Exception {
+        return 0;
     }
+    @Option(names = {"-f", "--format"},
+            description = "output format [default: stylish]",
+            paramLabel = "format")
+    File file;
+    @Parameters(index = "0",
+            description = "path to first file",
+            paramLabel = "filepath1")
+    String filepath1;
+    @Parameters(index = "0",
+            description = "path to second file",
+            paramLabel = "filepath2")
+    String filepath2;
 }
--- app/src/main/java/hexlet/code/Differ.java
+++ app/src/main/java/hexlet/code/Differ.java
@@ -1,5 +1,7 @@
 package hexlet.code;
 
+import picocli.CommandLine.Option;
+
 public class Differ {
     public static String generate(String filePath1, String filePath2) {
 //        var diff = Differ.generate(filePath1, filePath2);
